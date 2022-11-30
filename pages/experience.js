d66f2b517ca92e064975ed94fcde7b8632ad7a74
xiang <linruixiang123@gmail.com>
2022-12-01 05:06:30 +0800

    Complete Experience Page

--- pages/experience.js
+++ pages/experience.js
@@ -9,10 +9,86 @@ import {
   ListItem,
   useColorModeValue,
   chakra,
+  UnorderedList,
+  Divider
 } from '@chakra-ui/react';
+import Layout from '../components/layouts/article'
+import Paragraph from '../components/paragraph';
+import { BioYear } from '../components/bio'
+import content from '../public/content.json'
+
+const works = content.experience.work
+const competitions = content.experience.competitions
 
 const Experience = () => {
-  return <div>Experience page</div>;
+  const bgColor = useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')
+  return (
+    <Layout>
+      <Container>
+        <Heading variant='section-title'>
+          Work Experience
+        </Heading>
+        {works.map((work, index) => {
+          return (
+            <Box key={index}
+              borderRadius="lg"
+              mb={6}
+              p={3}
+              textAlign="left"
+              bg={bgColor}
+              css={{ backdropFilter: 'blur(10px)' }}
+            >
+              <Heading variant='subsection-title'>
+                {work.title}
+              </Heading>
+              <BioYear>{work.year}</BioYear>
+              <UnorderedList fontSize={16} fontWeight="light">
+                {work.description.map((text, index) => {
+                  return (
+                    <ListItem key={index}>
+                      {text}
+                    </ListItem>
+                  )
+                })}
+              </UnorderedList>
+            </Box>
+          )
+        })}
+        <Divider></Divider>
+        <Heading variant='section-title'>
+          Hackathons/Competitions
+        </Heading>
+        {
+          competitions.map((comp, index) => {
+            return (
+              <Box key={index}
+                borderRadius="lg"
+                mb={6}
+                p={3}
+                textAlign="left"
+                bg={bgColor}
+                css={{ backdropFilter: 'blur(10px)' }}
+              >
+                <Heading variant='subsection-title'>
+                  {comp.title}
+                </Heading>
+                <UnorderedList fontSize={16} fontWeight="light">
+                  {
+                    comp.description.map((text, index) => {
+                      return (
+                        <ListItem key={index}>
+                          {text}
+                        </ListItem>)
+                    })}
+                </UnorderedList>
+              </Box>
+            )
+          })}
+      </Container>
+    </Layout >
+
+  )
+
 };
 
 export default Experience;
--- pages/index.js
+++ pages/index.js
@@ -21,148 +21,151 @@ import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from 're
 // import thumbYouTube from '../public/images/links/youtube.png';
 // import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png';
 import Image from 'next/image';
+// import Resume from '../public/files/resume.pdf'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop),
 });
 
-const Home = () => (
-  <Layout>
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        css={{ backdropFilter: 'blur(10px)' }}
-      >
-        Hello, Welcome to my Homepage
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Ruixiang Lin
-          </Heading>
-          <p>Software Developer</p>
-        </Box>
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
-        >
-          <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
-          >
-            <ProfileImage
-              src="/images/test1.jpg"
-              alt="Profile image"
-              borderRadius="full"
-              width="100%"
-              height="100%"
-            />
-          </Box>
-        </Box>
-      </Box>
-
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          About Me
-        </Heading>
-        <Paragraph>
-          I am Rui Xiang, a penultimate Computer Science student in Nanyang
-          Technological University. I have passion in Tech and is eager to learn
-          about new technologies. I love to develop functional applications.
-        </Paragraph>
-
-        <Box align="center" my={4}>
-          {/* <NextLink href="/works" passHref scroll={false}> */}
-          <Button rightIcon={<DownloadIcon />} colorScheme="teal">
-            Résumé
-          </Button>
-          {/* </NextLink> */}
-        </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Have code with
-        </Heading>
+const Home = () => {
+  return (
+    <Layout>
+      <Container>
         <Box
           borderRadius="lg"
           mb={6}
           p={3}
-          textAlign="left"
+          textAlign="center"
           bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
           css={{ backdropFilter: 'blur(10px)' }}
         >
-          <Heading variant='section-content'>
-            Languages
+          Hello, Welcome to my Homepage
+        </Box>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Ruixiang Lin
+            </Heading>
+            <p>Software Developer</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
+          >
+            <Box
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              w="100px"
+              h="100px"
+              display="inline-block"
+              borderRadius="full"
+              overflow="hidden"
+            >
+              <ProfileImage
+                src="/images/test1.jpg"
+                alt="Profile image"
+                borderRadius="full"
+                width="100%"
+                height="100%"
+              />
+            </Box>
+          </Box>
+        </Box>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            About Me
           </Heading>
           <Paragraph>
-            Python, Javascript, Typescript, Java, C, HTML/CSS
-          </Paragraph>
-          <br />
-          <Heading variant='section-content'>
-            Frameworks/Libraries & Tools
-          </Heading>
-          <Paragraph>
-            React, Nextjs, Express, Nodejs, SpringBoot, Threejs, Prisma, MongoDB, PostgreSQL, Git, Prettier, NPM, Postman
+            I am Rui Xiang, a penultimate Computer Science student in Nanyang
+            Technological University. I have passion in Tech and is eager to learn
+            about new technologies. I love to develop functional applications.
           </Paragraph>
 
-        </Box>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Socials
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/xkryptios" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @xkryptios
+          <Box align="center" my={4}>
+            <a href='/files/resume.pdf' download='Ruixiang_Lin_Resume.pdf'>
+              <Button rightIcon={<DownloadIcon />} colorScheme="teal">
+                Résumé
               </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://www.linkedin.com/in/rui-xiang-lin-16a0a9206/" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoLinkedin />}
-              >
-                Ruixiang Lin
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://www.instagram.com/ahxiang_rx/" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @ahxiang_rx
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
-      </Section>
-    </Container>
-  </Layout>
-);
+            </a>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Have code with
+          </Heading>
+          <Box
+            borderRadius="lg"
+            mb={6}
+            p={3}
+            textAlign="left"
+            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+            css={{ backdropFilter: 'blur(10px)' }}
+          >
+            <Heading variant='subsection-title'>
+              Languages
+            </Heading>
+            <Paragraph>
+              Python, Javascript, Typescript, Java, C, HTML/CSS
+            </Paragraph>
+            <br />
+            <Heading variant='subsection-title'>
+              Frameworks/Libraries & Tools
+            </Heading>
+            <Paragraph>
+              React, Nextjs, Express, Nodejs, SpringBoot, Threejs, Prisma, MongoDB, PostgreSQL, Git, Prettier, NPM, Postman
+            </Paragraph>
+
+          </Box>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Socials
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://github.com/xkryptios" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoGithub />}
+                >
+                  @xkryptios
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://www.linkedin.com/in/rui-xiang-lin-16a0a9206/" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoLinkedin />}
+                >
+                  Ruixiang Lin
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://www.instagram.com/ahxiang_rx/" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoInstagram />}
+                >
+                  @ahxiang_rx
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
+      </Container>
+    </Layout>
+  );
+}
 
 export default Home;
 export { getServerSideProps } from '../components/chakra';
